ca5ad04e6a0ca7571c81f372c33c7000ef0433f9
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-10-28 10:35:18 -0500

    put and delete work for pet owners

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,29 +12,53 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
             return _context.PetOwners;  //new List<PetOwner>();
         }
 
+        //get by id
         [HttpGet("{id}")]
-        public PetOwner GetById(int id) {
+        public PetOwner GetById(int id)
+        {
             return _context.PetOwners
                 .SingleOrDefault(petowner => petowner.id == id);
         }
+        //post
         [HttpPost]
-        public PetOwner Post(PetOwner petowner){
+        public PetOwner Post(PetOwner petowner)
+        {
             _context.Add(petowner);
             _context.SaveChanges();
 
             return petowner;
         }
 
+        //delete
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            PetOwner petowner = _context.PetOwners.SingleOrDefault(petowner => petowner.id == id);
+
+            _context.PetOwners.Remove(petowner);
+            _context.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public PetOwner Put(int id, PetOwner petowner){
+            petowner.id = id;
+            
+            _context.Update(petowner);
+            _context.SaveChanges();
+            return petowner;
+        }
     }
 }
---
+++ Migrations/20211028151611_alterPetOwnersTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class alterPetOwnersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "petCount",
+                table: "PetOwners");
+
+            migrationBuilder.AddColumn<string>(
+                name: "emailAddress",
+                table: "PetOwners",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "emailAddress",
+                table: "PetOwners");
+
+            migrationBuilder.AddColumn<int>(
+                name: "petCount",
+                table: "PetOwners",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
